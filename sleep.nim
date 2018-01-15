fae5b6c2606396a9c050ff98b1ce7eb0de4c8241
Eric-Su <eric.suwh@gmail.com>
2018-01-15 22:47:42 +0800

    Add files via upload

--- sleep.nim
+++ sleep.nim
@@ -1,46 +1,120 @@
-import os, strutils, parseutils
+import os, strutils, parseutils, parseopt
+
+const
+  Version = "0.2"
 
 proc showUsageAndExit(rc = QuitFailure) =
   let sPrgName = extractFilename(getAppFilename())
   var    
     msg = ""
-  msg.add("Sleep v0.1\n\n" % sPrgName)
-  msg.add("Usage: $# n[d|h|m|s]\n" % sPrgName)
-  msg.add("       default n milliseconds to pause\n\n")
+  msg.add("Sleep v$#\n" % Version)
+  msg.add("Usage: $# [-options] n[d | h | m | s]\n" % sPrgName)
+  msg.add("       default n milliseconds to pause\n")
+  msg.add("where options include:\n")
+  msg.add("       -h | --help     print this help message\n")
+  msg.add("       -v | --verbose  enable verbose output\n")
   msg.add("Example:\n")
-  msg.add("       $# 3d\n" % sPrgName)
-  msg.add("       $# 4h\n" % sPrgName)
-  msg.add("       $# 5m\n" % sPrgName)
+  msg.add("       $# 900\n" % sPrgName)
+  msg.add("       $# 1h-0.99h\n" % sPrgName)
+  msg.add("       $# 3s0.2m .1h\n" % sPrgName)
   quit(msg, rc)
 
-proc main(): int {.discardable.} =
-  var sleeptime: int
-  var nLastIndex: cint
-  var p: int
+proc parseCmdLine(): int =
+  if paramCount() <= 0 :
+    showUsageAndExit()
 
-  if paramCount() >= 1 :
-    p = parseutils.parseInt(paramStr(1), sleeptime, 0)
-    if p == 0 or sleeptime == 0 : 
-      showUsageAndExit()
-    echo "paramStr(1)[p] = ", paramStr(1)[p]
-    case paramStr(1)[p]
-    of 'd':
-      sleeptime = sleeptime * 24 * 60 * 60 * 1000
-    of 'h':
-      sleeptime = sleeptime * 60 * 60 * 1000
-    of 'm':
-      sleeptime = sleeptime * 60 * 1000
-    of 's':
-      sleeptime = sleeptime * 1000
-    of '\0':
-      echo "got null"
-      discard
-    else:
-      showUsageAndExit()
-    echo "sleeptime = ", sleeptime
+  var
+    verbose = false
+    parm = initOptParser()
+
+  while true:
+    next(parm)
+    var 
+      kind = parm.kind
+      key = parm.key.string
+
+    case kind
+      of cmdArgument:
+        var
+          number: float
+          pos = 0
+        while pos < len(key):
+          let numLen = parseutils.parseFloat(key, number, pos)
+          if numLen == 0:
+            showUsageAndExit()
+          pos += numLen
+          case key[pos]
+            of 'd':
+              result += int(number * 24 * 60 * 60 * 1000)
+            of 'h':
+              result += int(number * 60 * 60 * 1000)
+            of 'm':
+              result += int(number * 60 * 1000)
+            of 's':
+              result += int(number * 1000)
+            of '\0':
+              result += int(number)
+            else:
+              showUsageAndExit()
+          pos += 1
+
+      of cmdLongoption, cmdShortOption:
+        case normalize(key)
+        of "h", "help":
+          showUsageAndExit(0)
+        of "v", "verbose":
+          verbose = true
+        else:
+          showUsageAndExit()
+      of cmdEnd: break
+
+  if verbose:
+    var
+      res: int
+      num: int
+      remainder: int = result
+      msg = ""
+
+    if result < 0:
+      msg = "Sleep 0ms "
+      msg.add(" ($# ms)" % $result)
+      echo msg
+      return
+
+    num = 24 * 60 * 60 * 1000
+    res = remainder div num
+    remainder = remainder mod num
+    if res > 0:
+      msg.add($res & "d")
+
+    num = 60 * 60 * 1000
+    res = remainder div num
+    remainder = remainder mod num
+    if res > 0:
+      msg.add($res & "h")
+
+    num = 60 * 1000
+    res = remainder div num
+    remainder = remainder mod num
+    if res > 0:
+      msg.add($res & "m")
+
+    num = 1000
+    res = remainder div num
+    remainder = remainder mod num
+    if res > 0:
+      msg.add($res & "s")
+
+    if msg == "" or remainder > 0:
+      msg.add($remainder & "ms")
+
+    if result > 1000:
+      msg.add(" ($# ms)" % $result)
+    echo "Sleep ", msg
+
+proc main() =
+  let sleeptime = parseCmdLine()
+  if sleeptime > 0:
     sleep(sleeptime)
-  else:
-    showUsageAndExit(0)
-  return 0
 
 main()
